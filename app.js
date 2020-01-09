89672b50c2bb4e219f44dca16d0aa7fc72257e4c
Cloud <BetaPhoenixSNSD@gmail.com>
2020-01-09 17:21:53 +0800

    新增表單驗證功能

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
+var flash = require('connect-flash');
 require('dotenv').config();
 
 var indexRouter = require('./routes/index');
@@ -19,6 +21,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(session({
+    secret: 'keyboard cat',
+    resave: false,
+    saveUninitialized: true,
+  }),
+);
+app.use(flash());
 
 app.use('/', indexRouter);
 app.use('/contact', contactRouter);
--- routes/contact.js
+++ routes/contact.js
@@ -2,40 +2,66 @@ const express = require('express');
 const router = express.Router(); 
 const nodemailer = require('nodemailer');
 const csrf = require('csurf');
+const { check, validationResult } = require('express-validator');
+
+var validat = [
+  check('email')
+    .isEmail()
+    .withMessage('Email 錯誤'),
+  check('username')
+    .isLength({ min: 2 })
+    .withMessage('姓名不可低於 3 個字元'),
+  check('description')
+    .isLength({ min: 10 })
+    .withMessage('字數不可少於 10 字元'),
+];
 
 // setup route middlewares
 var csrfProtection = csrf({ cookie: true })
 
 router.get('/', csrfProtection, function(req, res) {
-  res.render('contact', { csrfToken: req.csrfToken() });
+  res.render('contact', {
+    title: '信件發送頁面',
+    csrfToken: req.csrfToken(),
+    errorMessages: req.flash('errorMessages'),
+  });
 });
 router.get('/review', function(req, res) {
   res.render('contactReview');
 });
-router.post('/post', csrfProtection, function(req, res) {
+router.post('/post', [validat, csrfProtection], function(req, res) {
   const data = req.body;
   console.log(data);
-  const transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-      type: 'OAuth2',
-      user: process.env.NODEMAILER_USERNAME,
-      clientId: process.env.NODEMAILER_CLIENTID,
-      clientSecret: process.env.NODEMAILER_CLIENTSECRET,
-      refreshToken: process.env.NODEMAILER_REFRESHTOKEN,
-    },
-  });
-  const mailOptions = {
-    from: `"來自蘇揚傑"<${process.env.NODEMAILER_USERNAME}>`,
-    to: req.body.email,
-    subject: `${req.body.username}寄了一封測試信`,
-    text: req.body.description,
-  };
-  transporter.sendMail(mailOptions, (err, info) => {
-    if (err) {
-      return console.log(err);
-    }
-    res.redirect('/contact/review');
-  });
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    console.log('errorMessages', errors.array());
+    req.flash('errorMessages', errors.array());
+    res.redirect('/contact');
+  } else {
+    console.log('驗證通過，記送信件');
+    const transporter = nodemailer.createTransport({
+      service: 'Gmail',
+      auth: {
+        type: 'OAuth2',
+        user: process.env.NODEMAILER_USERNAME,
+        clientId: process.env.NODEMAILER_CLIENTID,
+        clientSecret: process.env.NODEMAILER_CLIENTSECRET,
+        refreshToken: process.env.NODEMAILER_REFRESHTOKEN,
+      },
+    });
+    const mailOptions = {
+      from: `"來自蘇揚傑"<${process.env.NODEMAILER_USERNAME}>`,
+      to: req.body.email,
+      subject: `${req.body.username}寄了一封測試信`,
+      text: req.body.description,
+    };
+    transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        console.log('錯誤訊息:' + err);
+        res.redirect('/contact');
+      }
+      res.redirect('/contact/review');
+    });
+  }
 });
 module.exports = router;
